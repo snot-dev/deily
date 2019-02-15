c8edabbda9fd3312d98832f88982e696015887ad
snot-dev <snot.dev@gmail.com>
2019-02-15 16:57:55 +0000

    changed eslint settings

--- src/client/App.js
+++ src/client/App.js
@@ -6,9 +6,9 @@ export default class App extends Component {
   state = { username: null };
 
   componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+    fetch( '/api/getUsername' )
+      .then( res => res.json() )
+      .then( user => this.setState( { username: user.username } ) );
   }
 
   render() {
