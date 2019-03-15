be0d0341fd3d17cc67a44b1103cfde594b7fb3b4
snot-dev <snot.dev@gmail.com>
2019-03-15 10:07:38 +0000

    fixed space and tab mixed issue

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
 import './app.css';
-import ReactImage from './react.png';
 
 export default class App extends Component {
-  state = { username: null };
+	state = { username: null };
 
-  // componentDidMount() {
-  //   fetch( '/api/getUsername' )
-  //     .then( res => res.json() )
-  //     .then( user => this.setState( { username: user.username } ) );
-  // }
-
-  render() {
-    const { username } = this.state;
-    return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please waitzz!</h1>}
-        <img src={ReactImage} alt="react" />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<h1>Deily!!</h1>
+			</div>
+		);
+	}
 }
