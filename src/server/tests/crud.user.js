70fc1f73c901412ea26b851e63709be5c0be3695
Snot <snot.dev@gmail.com>
2019-03-22 18:26:18 +0000

    removed vsCode from gitignore

--- src/server/tests/crud.user.js
+++ src/server/tests/crud.user.js
@@ -1,25 +1,58 @@
 const mongoose = require( 'mongoose' );
 const User = require( '../models/user' );
+const Team = require( '../models/team' );
 const SharedTests = require( '../shared/tests' );
 
 const tests = new SharedTests();
 const testName = "CRUD Users";
 const url = "/api/users/";
+const teamUrl = "/api/teams";
 
-const mock = {
+const user = {
 	_id: new mongoose.mongo.ObjectId( '56cb91bdc3464f14678934cb' ),
 	name: "test",
 	email: "test@test.com",
 	username: "test",
 	creator: false,
-	googleId: "thisIsAMockGoogleUserId",
-	penalties: []
+	googleId: "thisIsAMockGoogleUserId"
+};
+
+const team = {
+	_id: new mongoose.mongo.ObjectId( '56cb91bdc3464f14678936cb' ),
+	name: "User Test Team",
+	createdBy: '56cb91bdc3464f14274854cb'
 };
 
 const updateValue = "updatedTest";
 
-const copiedMock = Object.assign( {}, mock );
-const updatedMock = Object.assign( copiedMock, { name: updateValue } );
+const copiedUser = Object.assign( {}, user );
+const updatedUser = Object.assign( copiedUser, { name: updateValue } );
 
 // eslint-disable-next-line no-undef
-describe( testName, tests.CRUD( url, false, User, { new: mock, update: { mock: updatedMock, test: "name" } } ) );
+describe.only( testName, () => {
+	// console.warn( mock );
+
+	it( "Clean Up Team", tests.cleanUp( Team ) ); // eslint-disable-line no-undef
+
+	it( "Create Team before User", tests.createNew( teamUrl, Team, team ) ); // eslint-disable-line no-undef
+
+	it( "Clean Up User", tests.cleanUp( User ) ); // eslint-disable-line no-undef
+
+	it( "Create new Item", ( done ) => {
+		this._createNew( url, Model, payload, validation )
+			.then( () => {
+				done();
+			} )
+			.catch( this._failTest( done ) );
+	}
+
+	it( "List all items", tests.getAll( url, User ) ); // eslint-disable-line no-undef
+
+	it( "List one item", tests.getById( url, User, user._id ) ); // eslint-disable-line no-undef
+
+	it( "Update item field", tests.update( url, User, updatedUser ) ); // eslint-disable-line no-undef
+
+	it( 'Delete Created Item', tests.deleteById( url, User, user._id ) ); // eslint-disable-line no-undef
+
+	// it( "Clean Up Team", tests.cleanUp( Team ) ); // eslint-disable-line no-undef
+} );
